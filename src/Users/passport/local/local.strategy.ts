9e56975060000cd8689dce9f78b84577a317c40c
Seung-Cheol <rlatmdcjf10@naver.com>
2023-06-24 17:10:49 +0900

    asdasfgsd

--- src/Users/passport/local/local.strategy.ts
+++ src/Users/passport/local/local.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { AuthService } from '../../auth.service';
+import { AuthService } from '../../oauth2.service';
 import { UserEntity} from '../../user.entity'
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
