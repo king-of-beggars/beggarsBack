b65a5133df03bc69b871ac64bbd1d4d04a2fc7a0
comecomeDev <jinga0908@gmail.com>
2023-07-03 21:48:05 +0900

    refresh 테스트

--- src/Users/passport/refresh/refresh.guard.ts
+++ src/Users/passport/refresh/refresh.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class RefreshAuthenticationGuard extends AuthGuard('refresh') {}
+export class RefreshAuthenticationGuard extends AuthGuard('refreshToken') {}
--- src/Users/passport/refresh/refresh.strategy.ts
+++ src/Users/passport/refresh/refresh.strategy.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
-
+//import { Strategy } from 'passport-jwt';
+// import {} from 'passport-naver';
+import { Request } from 'express';
+import { UserService } from 'src/Users/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { TokenDto } from './../../dto/token.dto';
 @Injectable()
-export class RefreshStrategy extends PassportStrategy(Strategy) {
-    
+export class RefreshStrategy extends PassportStrategy(
+  Strategy,
+  'refreshToken',
+) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          let token = null;
+          if (request && request.cookies) {
+            token = request.cookies['refreshToken'].split(' ')[1];
+          }
+          return token;
+        },
+      ]),
+      secretOrKey: 'process.env.REFRESH_TIME',
+      passReqToCallback: true,
+    });
+  }
+  async validate(request: Request, payload: any) {
+    const refreshToken = request.cookies['refreshToken'].split(' ')[1];
 
+    return this.userService.getUserIfRefreshTokenMatches(
+      refreshToken,
+      payload.userId,
+    );
+  }
 }
