b659631f72b3a84a3a7d5a3d971d304b6f68dade
comecomeDev <jinga0908@gmail.com>
2023-07-05 22:09:15 +0900

    frame modify

--- src/Cashlists/cashbook.controller.ts
+++ src/Cashlists/cashbook.controller.ts
@@ -1,6 +1,18 @@
-import { Controller,Post,Req, Body, HttpCode, UseGuards, Get, Query, Delete, Param, Put} from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  Body,
+  HttpCode,
+  UseGuards,
+  Get,
+  Query,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
 import { CashbookService } from './cashbook.service';
-import {Cronjob} from 'cron'
+import { Cronjob } from 'cron';
 import { PostDetailDto } from './dto/postDetail.dto';
 import { CashbookEntity } from './entity/cashbook.entity';
 import { AccessAuthenticationGuard } from 'src/Users/passport/jwt/access.guard';
@@ -11,114 +23,131 @@ import { FrameDto } from './dto/frame.dto';
 
 @Controller('api/cashbook')
 export class CashbookContoller {
-    constructor(
-        private readonly cashbookService : CashbookService,
-        private readonly userService : UserService
-    ){}
+  constructor(
+    private readonly cashbookService: CashbookService,
+    private readonly userService: UserService,
+  ) {}
 
-    @Get('/main')
-    @UseGuards(AccessAuthenticationGuard)
-    async mainPage(@Req() req : any) {
-        const { user } = req 
-        const date : string = new Date().toISOString().split('T')[0]
-        const nowdate : Date = new Date(date)
-        //1. 몇 일 째 되는 날
-        const dateValue : number = await this.userService.userSignupDate(user.userId)
-        console.log(dateValue)
-        //2. 2주 데이터, 남은 날은 null 처리
-        const twoweek = await this.cashbookService.getCashbookDuringDate(nowdate,user.userId)
+  @Get('/main')
+  @UseGuards(AccessAuthenticationGuard)
+  async mainPage(@Req() req: any) {
+    const { user } = req;
+    const date: string = new Date().toISOString().split('T')[0];
+    const nowdate: Date = new Date(date);
+    //1. 몇 일 째 되는 날
+    const dateValue: number = await this.userService.userSignupDate(
+      user.userId,
+    );
+    console.log(dateValue);
+    //2. 2주 데이터, 남은 날은 null 처리
+    const twoweek = await this.cashbookService.getCashbookDuringDate(
+      nowdate,
+      user.userId,
+    );
 
-        //3. 당일 유저 별, 섹션 별 총합목표, 총합소비
-        const totalValue = await this.cashbookService.getCashbookByDate(nowdate,user.userId)
-        console.log(totalValue)
-        return `data : {
+    //3. 당일 유저 별, 섹션 별 총합목표, 총합소비
+    const totalValue = await this.cashbookService.getCashbookByDate(
+      nowdate,
+      user.userId,
+    );
+    console.log(totalValue);
+    return `data : {
             signupDay : ${dateValue},
             twoweek : ${twoweek},
             totalValue : ${totalValue}
-        }`
+        }`;
+  }
+
+  @Post('frame')
+  async cashFrameCreate(@Body() body: FrameDto, @Req() req: any) {
+    const { user } = req;
+    let frameDto = new FrameDto();
+    frameDto = {
+      cashCategory: body.cashCategory,
+      cashName: body.cashName,
+      cashListGoalValue: body.cashListGoalValue,
+      userId: user.userId,
+    };
+
+    //이 부분 트랜잭션 해야하는데 어떡하지
+    const frame = await this.cashbookService.frameCreate(frameDto);
+
+    return `프레임 생성 완료`;
+  }
+
+  //프레임 수정
+  @Put('frame/:cashListId')
+  async cashFrameUpdate(@Body() body: FrameDto, @Req() req: any) {
+    // const { user } = req;
+
+    let frameDto = new FrameDto();
+    frameDto = {
+      cashCategory: body.cashCategory,
+      cashName: body.cashName,
+      cashListGoalValue: body.cashListGoalValue,
+      userId: body.userId,
+    };
+
+    const frame = await this.cashbookService.frameModify(frameDto);
+    return '프레임 수정 완료';
+  }
+
+  //디폴트는 오늘로 전달해주시길 프론트엔드 2023-05-24 형식으로
+  @Get('/')
+  @UseGuards(AccessAuthenticationGuard)
+  async cashList(@Query() query, @Req() req: any) {
+    const { user } = req;
+    const date: Date = query.date;
+    const result = this.cashbookService.getCashbookByDate(date, user.userId);
+    return `data : ${result}`;
+  }
+
+  @Get(':cashbookId')
+  async cashDetail(@Param() params: number) {
+    const cashbookId = params;
+    const result = await this.cashbookService.getDetail(cashbookId);
+    if (!result) {
+      throw new Error('정상적으로 불러올 수 없습니다');
     }
+    return `data : ${result}`;
+  }
 
+  @Post(':cashDetailId')
+  async postDetail(
+    @Param() params: CashbookEntity,
+    @Body() body: PostDetailDto,
+  ) {
+    let postDetailDto = new PostDetailDto();
+    postDetailDto = {
+      cashbookId: params,
+      cashDetailText: body.cashDetailText,
+      cashDetailValue: body.cashDetailValue,
+    };
 
-    @Post('frame')
-    async cashFrameCreate (@Body() body : FrameDto, @Req() req : any) {
-        const { user } = req
-        let frameDto = new FrameDto()
-        frameDto = {
-            cashCategory : body.cashCategory,
-            cashName : body.cashName,
-            cashListGoalValue : body.cashListGoalValue,
-            userId : user.userId
-        }
-
-
-        //이 부분 트랜잭션 해야하는데 어떡하지
-        const frame = await this.cashbookService.frameCreate(frameDto)
-        
-        return `프레임 생성 완료`
+    const result = await this.cashbookService.postDetail(postDetailDto);
+    if (!result) {
+      throw new Error('정상적으로 입력되지 않았습니다');
     }
+    return `입력 성공`;
+  }
 
-    @Put('frame/:cashListId')
-    async cashFrameUpdate (@Req() req : any) {
-        const { user } = req
-
+  @Delete(':cashDetailId')
+  @UseGuards(AccessAuthenticationGuard)
+  async deleteDetail(@Param() cashDetailId: CashDetailEntity, @Req() req: any) {
+    const { user } = req;
+    if (!cashDetailId) {
+      throw new Error('정상적으로 요청되지 않았습니다');
     }
+    const detail = await this.cashbookService.getOneDetail(cashDetailId);
+    let valueUpdateDto = new ValueUpdateDto();
+    valueUpdateDto = {
+      cashbookId: detail.cashbookId,
+      cashDetailValue: -Number(detail.cashDetailValue),
+    };
 
-    //디폴트는 오늘로 전달해주시길 프론트엔드 2023-05-24 형식으로
-    @Get('/')
-    @UseGuards(AccessAuthenticationGuard)
-    async cashList(@Query() query, @Req() req : any) {
-        const { user } = req
-        const date : Date = query.date
-        const result = this.cashbookService.getCashbookByDate(date,user.userId)
-        return `data : ${result}`
-    }
+    await this.cashbookService.addValue(valueUpdateDto);
+    await this.cashbookService.deleteDetail(cashDetailId);
 
-    @Get(':cashbookId')
-    async cashDetail(@Param() params : number) {
-        const cashbookId = params
-        const result = await this.cashbookService.getDetail(cashbookId)
-        if(!result) {
-
-            throw new Error('정상적으로 불러올 수 없습니다')
-        }
-        return `data : ${result}`
-    }
-
-    @Post(":cashDetailId")
-    async postDetail(@Param() params : CashbookEntity, @Body() body : PostDetailDto) {
-
-        let postDetailDto = new PostDetailDto();
-        postDetailDto = {
-            cashbookId : params,
-            cashDetailText : body.cashDetailText,
-            cashDetailValue : body.cashDetailValue
-        };
-
-        const result = await this.cashbookService.postDetail(postDetailDto);
-        if(!result) {
-            throw new Error('정상적으로 입력되지 않았습니다')
-        }
-        return `입력 성공`
-    }
-
-    @Delete(":cashDetailId")
-    @UseGuards(AccessAuthenticationGuard)
-    async deleteDetail(@Param() cashDetailId : CashDetailEntity, @Req() req : any) {
-        const { user } = req
-        if(!cashDetailId) {
-            throw new Error('정상적으로 요청되지 않았습니다')
-        }
-        const detail = await this.cashbookService.getOneDetail(cashDetailId)
-        let valueUpdateDto = new ValueUpdateDto()
-        valueUpdateDto = {
-            cashbookId : detail.cashbookId,
-            cashDetailValue : -Number(detail.cashDetailValue)
-        }
-
-        await this.cashbookService.addValue(valueUpdateDto)
-        await this.cashbookService.deleteDetail(cashDetailId)
-
-        return `삭제 성공`
-    }
-    
+    return `삭제 성공`;
+  }
 }
--- src/Cashlists/cashbook.service.ts
+++ src/Cashlists/cashbook.service.ts
@@ -1,186 +1,241 @@
-import { Injectable } from "@nestjs/common";
-import { Repository, Join  } from "typeorm";
-import { CashDetailEntity } from "./entity/cashDetail.entity";
-import { CashbookEntity } from "./entity/cashbook.entity";
-import { InjectRepository ,  } from "@nestjs/typeorm";
-import { PostDetailDto } from "./dto/postDetail.dto";
-import UserEntity from "src/Users/user.entity";
-import { ValueUpdateDto } from "./dto/valueUpdate.dto";
-import { FrameDto } from "./dto/frame.dto";
-import { CashListEntity } from "./entity/cashList.entity";
-import { CashActivityEntity } from "./entity/cashactivity.entity";
+import { Injectable } from '@nestjs/common';
+import { Repository, Join } from 'typeorm';
+import { CashDetailEntity } from './entity/cashDetail.entity';
+import { CashbookEntity } from './entity/cashbook.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostDetailDto } from './dto/postDetail.dto';
+import UserEntity from 'src/Users/user.entity';
+import { ValueUpdateDto } from './dto/valueUpdate.dto';
+import { FrameDto } from './dto/frame.dto';
+import { CashListEntity } from './entity/cashList.entity';
+import { CashActivityEntity } from './entity/cashactivity.entity';
 
 @Injectable()
 export class CashbookService {
-    constructor(
-        @InjectRepository(CashDetailEntity)
-        private readonly cashDetailEntity : Repository<CashDetailEntity>,
-        @InjectRepository(CashbookEntity)
-        private readonly cashbookEntity : Repository<CashbookEntity>,
-        @InjectRepository(CashListEntity)
-        private readonly cashListEntity : Repository<CashListEntity>,
-        @InjectRepository(CashActivityEntity)
-        private readonly cashactivityEntity : Repository<CashActivityEntity>
-    ){}
+  constructor(
+    @InjectRepository(CashDetailEntity)
+    private readonly cashDetailEntity: Repository<CashDetailEntity>,
+    @InjectRepository(CashbookEntity)
+    private readonly cashbookEntity: Repository<CashbookEntity>,
+    @InjectRepository(CashListEntity)
+    private readonly cashListEntity: Repository<CashListEntity>,
+    @InjectRepository(CashActivityEntity)
+    private readonly cashactivityEntity: Repository<CashActivityEntity>,
+  ) {}
 
-    async getcashbookAndDetail(cashbookId : unknown) : Promise<CashbookEntity> {
+  async getcashbookAndDetail(cashbookId: unknown): Promise<CashbookEntity> {
+    return await this.cashbookEntity
+      .createQueryBuilder('cashbook')
+      .innerJoinAndSelect('cashbook.detail', 'cashDetail')
+      .innerJoinAndSelect('cashbook.userId', 'user.userId')
+      .where('cashbook.cashbookId=:cashbookId', { cashbookId })
+      .getOne();
+  }
 
-        return await this.cashbookEntity
-        .createQueryBuilder('cashbook')
-        .innerJoinAndSelect('cashbook.detail','cashDetail')
-        .innerJoinAndSelect('cashbook.userId','user.userId')
-        .where('cashbook.cashbookId=:cashbookId', {cashbookId})
-        .getOne()
-        
-    }
+  async getDetail(cashbookId: number): Promise<CashDetailEntity[]> {
+    return await this.cashDetailEntity
+      .createQueryBuilder('cashDetail')
+      .where('cashDetail.cashbookId=:cashbookId', { cashbookId })
+      .orderBy('cashDetail.cashDetailCreatedAt', 'DESC')
+      .getMany();
+  }
 
-    async getDetail(cashbookId : number) : Promise<CashDetailEntity[]> {
+  async postDetail(postDetailDto: PostDetailDto): Promise<any> {
+    const query = this.cashDetailEntity.create(postDetailDto);
 
-        return await this.cashDetailEntity
+    let valueUpdateDto = new ValueUpdateDto();
+    valueUpdateDto = {
+      cashbookId: postDetailDto.cashbookId,
+      cashDetailValue: postDetailDto.cashDetailValue,
+    };
+    await this.addValue(valueUpdateDto);
+    return await this.cashDetailEntity.save(query);
+  }
+
+  async deleteDetail(cashDetailId: CashDetailEntity): Promise<any> {
+    try {
+      return this.cashDetailEntity
         .createQueryBuilder('cashDetail')
-        .where('cashDetail.cashbookId=:cashbookId',{cashbookId})
-        .orderBy('cashDetail.cashDetailCreatedAt',"DESC")
-        .getMany()
+        .delete()
+        .where('cashDetail.cashDetailId', { cashDetailId });
+    } catch (e) {
+      throw new Error('DB접속에러');
+    }
+  }
+
+  async getCashbookByDate(
+    date: Date,
+    userId: UserEntity,
+  ): Promise<CashbookEntity[]> {
+    return await this.cashbookEntity
+      .createQueryBuilder('cashbook')
+      .select()
+      .where('DATE(cashbook.cashbookCreatedAt)=DATE(:date)', { date })
+      .andWhere('cashbook.userId=:userId', { userId })
+      .orderBy('cashbook.cashbookCreatedAt', 'DESC')
+      .getMany();
+  }
+
+  async addValue(valueUpdate: ValueUpdateDto): Promise<any> {
+    return await this.cashbookEntity
+      .createQueryBuilder('cashbook')
+      .update()
+      .set({
+        cashbookNowValue: () =>
+          `cashbookNowValue + ${valueUpdate.cashDetailValue}`,
+      })
+      .where('cashbookId=:cashbookId', { cashbookId: valueUpdate.cashbookId })
+      .execute();
+  }
+
+  async getCashbookDuringDate(
+    endDate: Date,
+    userId: UserEntity,
+  ): Promise<CashDetailEntity[]> {
+    const day: number = endDate.getDay() + 7 + 1;
+    let startDate = new Date();
+    startDate.setDate(endDate.getDate() - day);
+    const query = await this.cashbookEntity
+      .createQueryBuilder('cashbook')
+      .select([
+        'date(cashbookCreatedAt) as cashbookCreatedAt',
+        'cashbookCategory',
+        'SUM(cashbookNowValue) as cashbookNowValue',
+        'SUM(cashbookGoalValue) as cashbookGoalValue',
+      ])
+      .where('cashbookCreatedAt > :startDate', { startDate })
+      .andWhere('cashbookCreatedAt <= :endDate', { endDate })
+      .andWhere('userId=:userId', { userId })
+      .groupBy('date(cashbookCreatedAt)')
+      .addGroupBy('cashbookCategory')
+      .getMany();
+    console.log(`####${query}`);
+    let array = new Array(14).fill(null);
+    let result = array.map((_, e) => {
+      let date = new Date();
+      date.setDate(date.getDate() - e);
+      let string = date.toISOString().split('T')[0];
+      let obj = {};
+      obj[string] = null;
+      return obj;
+    });
+    result = result.reverse();
+    let trueResult = Object.assign({}, ...result);
+    for (let i = 0; i < query.length; i++) {
+      const querydate = query[i].cashbookCreatedAt.toISOString().split('T')[0];
+      console.log(querydate);
+      query[i].cashbookGoalValue >= query[i].cashbookNowValue
+        ? (trueResult[querydate] = true)
+        : (trueResult[querydate] = false);
+    }
+    return trueResult;
+  }
+
+  async getOneDetail(
+    cashDetailId: CashDetailEntity,
+  ): Promise<CashDetailEntity> {
+    return await this.cashDetailEntity
+      .createQueryBuilder('cashDetail')
+      .select()
+      .where('cashDetailId=:cashDetailId', { cashDetailId })
+      .getOne();
+  }
+
+  async frameActivityCreate(cashEntity: CashListEntity) {
+    let date = new Date();
+
+    const query = this.cashactivityEntity.create({
+      cashListId: cashEntity,
+      cashRestartDate: date,
+      cashUpdateDate: date,
+    });
+
+    return await this.cashactivityEntity.save(query);
+  }
+
+  async cashbookCreate(frameDto: FrameDto) {
+    const query = this.cashbookEntity.create({
+      cashbookCategory: frameDto.cashCategory,
+      cashbookName: frameDto.cashName,
+      cashbookGoalValue: frameDto.cashListGoalValue,
+    });
+    return await this.cashbookEntity.save(query);
+  }
+
+  async frameCreate(frameDto: FrameDto) {
+    if (!frameDto) {
+      throw new Error('서비스 단으로 데이터가 넘어오지 않음');
     }
 
-    async postDetail(postDetailDto : PostDetailDto) : Promise<any> {
-        const query = this.cashDetailEntity
-        .create(
-            postDetailDto
-        )
+    const frame = this.cashListEntity.create(frameDto);
 
-        let valueUpdateDto = new ValueUpdateDto()
-        valueUpdateDto = {
-            cashbookId : postDetailDto.cashbookId,
-            cashDetailValue : postDetailDto.cashDetailValue
-        }
-        await this.addValue(valueUpdateDto)
-        return await this.cashDetailEntity.save(query)
+    const query: CashListEntity = await this.cashListEntity.save(frame);
+
+    if (!frame) {
+      throw new Error('프레임 생성 에러');
     }
 
-    async deleteDetail(cashDetailId : CashDetailEntity) : Promise<any> {
-        try {
-            return this.cashDetailEntity
-            .createQueryBuilder('cashDetail')
-            .delete()
-            .where('cashDetail.cashDetailId', {cashDetailId})
-        } catch(e) {
-            throw new Error('DB접속에러')
-        }
+    const activity = await this.frameActivityCreate(query);
+    if (!activity) {
+      throw new Error('액티비티 생성 에러');
     }
-
-    async getCashbookByDate(date : Date, userId : UserEntity) : Promise<CashbookEntity[]> {
-         return await this.cashbookEntity
-        .createQueryBuilder('cashbook')
-        .select()
-        .where('DATE(cashbook.cashbookCreatedAt)=DATE(:date)',{date})
-        .andWhere('cashbook.userId=:userId',{userId})
-        .orderBy('cashbook.cashbookCreatedAt','DESC')
-        .getMany()
-        
+    const cashbook = await this.cashbookCreate(frameDto);
+    if (!cashbook) {
+      throw new Error('캐시북 생성 에러');
     }
+    return frame;
+  }
 
-    async addValue(valueUpdate : ValueUpdateDto) : Promise<any> {
-         return await this.cashbookEntity
-        .createQueryBuilder('cashbook')
+  async allCashlist(): Promise<CashListEntity[]> {
+    return await this.cashListEntity
+      .createQueryBuilder('cashlist')
+      .select()
+      .getMany();
+  }
+
+  /**
+   * @description frameModify
+   * @param frameDto
+   * @returns
+   */
+  async frameModify(frameDto: FrameDto) {
+    if (!frameDto) {
+      throw new Error('서비스 단으로 데이터가 넘어오지 않음');
+    }
+    try {
+      const cashList = await this.cashListEntity
+        .createQueryBuilder('CashList')
+        .innerJoin('CashList.userId', 'User')
+        .where('CashList.userId = :userId', { userId: frameDto.userId })
+        .getOne();
+
+      //카테고리 정보 검색
+      const cashactivity = await this.cashactivityEntity
+        .createQueryBuilder('cashactivity')
+        .innerJoin('cashactivity.cashListId', 'CashList')
+        .where('cashactivity.cashListId = :cashListId', {
+          cashListId: cashList.cashListId,
+        })
+        .getOne();
+      if (
+        (await cashactivity).cashStopDate > (await cashactivity).cashRestartDate
+      ) {
+        throw new Error('비활성화');
+      }
+
+      const frame = await this.cashListEntity
+        .createQueryBuilder('CashList')
         .update()
-        .set({cashbookNowValue : () => `cashbookNowValue + ${valueUpdate.cashDetailValue}`})
-        .where('cashbookId=:cashbookId',{cashbookId : valueUpdate.cashbookId})
-        .execute()
-    }
-
-    async getCashbookDuringDate(endDate : Date, userId : UserEntity) : Promise<CashDetailEntity[]>  {
-        const day : number = endDate.getDay() + 7 + 1
-        let startDate = new Date();
-        startDate.setDate(endDate.getDate() - day)
-        const query = await this.cashbookEntity
-        .createQueryBuilder('cashbook')
-        .select(['date(cashbookCreatedAt) as cashbookCreatedAt','cashbookCategory','SUM(cashbookNowValue) as cashbookNowValue','SUM(cashbookGoalValue) as cashbookGoalValue'])
-        .where('cashbookCreatedAt > :startDate',{startDate})
-        .andWhere('cashbookCreatedAt <= :endDate',{endDate})
-        .andWhere('userId=:userId',{userId})
-        .groupBy('date(cashbookCreatedAt)')
-        .addGroupBy('cashbookCategory')
-        .getMany()
-        console.log(`####${query}`)
-        let array = new Array(14).fill(null)
-        let result = array.map((_, e)=>{
-            let date = new Date()
-            date.setDate(date.getDate() - e)
-            let string = date.toISOString().split('T')[0]
-            let obj = {}
-            obj[string] = null
-            return obj
+        .set({
+          cashCategory: frameDto.cashCategory,
+          cashName: frameDto.cashName,
+          cashListGoalValue: frameDto.cashListGoalValue,
         })
-        result = result.reverse()
-        let trueResult = Object.assign({}, ...result)
-        for(let i=0; i<query.length; i++) {
-            const querydate = query[i].cashbookCreatedAt.toISOString().split('T')[0]
-            console.log(querydate)
-            query[i].cashbookGoalValue >=query[i].cashbookNowValue ? trueResult[querydate] = true : trueResult[querydate] = false
-        }
-        return trueResult;
+        .where('cashListId=:cashListId', { cashListId: cashList.cashListId })
+        .execute();
+      return frame;
+    } catch (error) {
+      throw new Error('예기치 못한 에러로 수정 불가');
     }
-
-    async getOneDetail(cashDetailId : CashDetailEntity) : Promise<CashDetailEntity> {
-        return await this.cashDetailEntity
-        .createQueryBuilder('cashDetail')
-        .select()
-        .where('cashDetailId=:cashDetailId',{cashDetailId})
-        .getOne()
-    }
-    
-    async frameActivityCreate(cashEntity : CashListEntity) {
-        let date = new Date()
-
-        const query =  this.cashactivityEntity.create({
-            cashListId : cashEntity,
-            cashRestartDate : date,
-            cashUpdateDate : date
-        })
-
-        return await this.cashactivityEntity.save(query)
-    }
-
-    async cashbookCreate(frameDto : FrameDto) {
-        const query = this.cashbookEntity.create({
-            cashbookCategory : frameDto.cashCategory,
-            cashbookName : frameDto.cashName,
-            cashbookGoalValue : frameDto.cashListGoalValue
-        })
-        return await this.cashbookEntity.save(query)
-
-    }
-
-    async frameCreate(frameDto : FrameDto) {
-        if(!frameDto) {
-            throw new Error('서비스 단으로 데이터가 넘어오지 않음')
-        }
-
-        const frame  = this.cashListEntity.create(
-            frameDto
-        )
-        const query : CashListEntity = await this.cashListEntity.save(frame)
-
-        if(!frame) {
-            throw new Error('프레임 생성 에러')
-        }
-
-        const activity = await this.frameActivityCreate(query)
-        if(!activity) {
-            throw new Error('액티비티 생성 에러')
-        }
-        const cashbook = await this.cashbookCreate(frameDto)
-        if(!cashbook) {
-            throw new Error('캐시북 생성 에러')
-        }
-        return frame
-    }
-
-    async allCashlist() : Promise<CashListEntity[]> {
-        return await this.cashListEntity
-        .createQueryBuilder('cashlist')
-        .select()
-        .getMany()
-    }
+  }
 }
