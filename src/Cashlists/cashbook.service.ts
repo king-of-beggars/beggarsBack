2a481884544be0e868555598f702d45f11a9138b
Seung-Cheol <rlatmdcjf10@naver.com>
2023-07-08 12:44:17 +0900

    일부 폴더구조 변경

--- src/Cashlists/cashbook.service.ts
+++ src/Cashlists/cashbook.service.ts
@@ -68,6 +68,7 @@ export class CashbookService {
             .createQueryBuilder('cashDetail')
             .delete()
             .where('cashDetail.cashDetailId', {cashDetailId})
+            .execute()
         } catch(e) {
             
         }
