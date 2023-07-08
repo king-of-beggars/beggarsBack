c7e6a9faca41b8fc37c4356ed0ef21db0d820ac8
Seung-Cheol <rlatmdcjf10@naver.com>
2023-07-08 11:06:36 +0900

    dsdsf

--- src/Cashlists/autocreate.service.ts
+++ src/Cashlists/autocreate.service.ts
@@ -13,7 +13,7 @@ export class AutoCreateService {
         private cashbookEntity : Cashbook
     ){}
 
-    @Cron(CronExpression.EVERY_MINUTE)
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
     async autoCashbook() {
         const list : CashList[] = await this.cashbookService.allCashlist()
         for(let i=0; i<list.length; i++) {
